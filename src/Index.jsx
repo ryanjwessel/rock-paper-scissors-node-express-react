dad9821f9f95362afb1e8f35fc0bf2e708968923
Ryan Wessel <rynjwssl@gmail.com>
2018-08-24 17:36:29 -0400

    update scripts so that there is a separate command to only run webpack, and another that runs webpack then initiates the server. start to add component for selecting your choice for the game.

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import { render } from 'react-dom';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+
+// Styles
+import './main.scss';
+
+const Choice = ( props ) => {
+	return (
+		<button
+			type="button"
+		>
+			{ props.name }
+		</button>
+	);
+};
 
 const App = () => {
+	const choices = [ 'rock', 'paper', 'scissors' ];
+
 	return (
-		<p>Beginnings of a React app.</p>
-	)
+		<div className="container">
+			<h1>Rock Paper Scissors!</h1>
+			<h2>Choose your weapon...</h2>
+			{
+				choices.map( item => {
+					return <Choice key={ item } name={ item } />;
+				})
+			}
+		</div>
+	);
 };
 
 render(
